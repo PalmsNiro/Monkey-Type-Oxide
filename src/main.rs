d4b12dfc8ce87c5aa73749608dcf7da670393042
PalmsNiro <sebi.herr13@gmail.com>
2024-11-01 16:29:58 +0100

    put back logger

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,23 @@ use ratatui::prelude::*;
 use std::io;
 // use std::thread::Builder;
 
+use std::fs::File;
+use log::{info, error, debug, warn};
+use env_logger::Builder;
+use chrono::Local;
+use std::io::Write;
 
 
 use crate::app::App;
 
 fn main() -> io::Result<()> {
-    
+    // setup logger
+    if let Err(e) = setup_logger() {
+        eprintln!("Failed to set up logger: {}", e);
+        return Ok(());
+    }
+    info!("Startet application");
+
     crossterm::terminal::enable_raw_mode()?;
     let mut stdout = io::stdout();
     crossterm::execute!(stdout, crossterm::terminal::EnterAlternateScreen)?;
@@ -30,4 +41,23 @@ fn main() -> io::Result<()> {
     crossterm::execute!(io::stdout(), crossterm::terminal::LeaveAlternateScreen)?;
 
     result
+}
+
+fn setup_logger() -> Result<(), Box<dyn std::error::Error>> {
+    let logfile = File::create(format!("logs/game_log_{}.txt", Local::now().format("%Y-%m-%d_%H-%M-%S")))?;
+
+    Builder::new()
+        .target(env_logger::Target::Pipe(Box::new(logfile)))
+        .filter_level(log::LevelFilter::Debug)
+        .format(|buf, record| {
+            writeln!(buf,
+                "{} [{}] - {}",
+                Local::now().format("%Y-%m-%d %H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .init();
+
+    Ok(())
 }
